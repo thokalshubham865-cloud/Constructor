4a3a523458f7b27fde7283a632c505bda5ce858b
Shubham Thokal <thokalshubham865@gmail.com>
2026-01-28 17:15:16 +0530

    Add files via upload

---
+++ MainConstructor.java
@@ -0,0 +1,26 @@
+package Constructor;
+
+public class MainConstructor {
+
+	public static void main(String[] args) {
+		
+	
+	Welcome wel = new Welcome();
+	System.out.println();
+	
+	Student st = new Student();
+	System.out.println();
+	
+	End en = new End();
+	
+	System.out.println();
+	Employee em = new Employee();
+	
+	System.out.println();
+	Addition ad = new Addition();
+	
+	DefaultConstructor de = new DefaultConstructor();
+	de.constructorDefault();
+	System.out.println();
+   }
+}
